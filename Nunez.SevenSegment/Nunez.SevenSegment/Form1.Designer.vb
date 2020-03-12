9b9b1e1b66b7fcd9314a00e0791fce66c0eb71db
Darlyn <35473002+DarlynNunez@users.noreply.github.com>
2020-03-12 22:24:43 +0800

    Add files via upload

---
+++ Nunez.SevenSegment/Nunez.SevenSegment/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Shape1 = New System.Windows.Forms.Button()
+        Me.Shape7 = New System.Windows.Forms.Button()
+        Me.Shape2 = New System.Windows.Forms.Button()
+        Me.Shape3 = New System.Windows.Forms.Button()
+        Me.Shape4 = New System.Windows.Forms.Button()
+        Me.Shape5 = New System.Windows.Forms.Button()
+        Me.Shape6 = New System.Windows.Forms.Button()
+        Me.GO = New System.Windows.Forms.Button()
+        Me.RB0 = New System.Windows.Forms.RadioButton()
+        Me.RB1 = New System.Windows.Forms.RadioButton()
+        Me.RB2 = New System.Windows.Forms.RadioButton()
+        Me.RB3 = New System.Windows.Forms.RadioButton()
+        Me.RB4 = New System.Windows.Forms.RadioButton()
+        Me.RB5 = New System.Windows.Forms.RadioButton()
+        Me.RB6 = New System.Windows.Forms.RadioButton()
+        Me.RB7 = New System.Windows.Forms.RadioButton()
+        Me.RB8 = New System.Windows.Forms.RadioButton()
+        Me.RB9 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'Shape1
+        '
+        Me.Shape1.BackColor = System.Drawing.Color.White
+        Me.Shape1.Cursor = System.Windows.Forms.Cursors.No
+        Me.Shape1.Location = New System.Drawing.Point(80, 43)
+        Me.Shape1.Name = "Shape1"
+        Me.Shape1.Size = New System.Drawing.Size(120, 28)
+        Me.Shape1.TabIndex = 0
+        Me.Shape1.UseVisualStyleBackColor = False
+        '
+        'Shape7
+        '
+        Me.Shape7.BackColor = System.Drawing.Color.White
+        Me.Shape7.Cursor = System.Windows.Forms.Cursors.No
+        Me.Shape7.Location = New System.Drawing.Point(80, 182)
+        Me.Shape7.Name = "Shape7"
+        Me.Shape7.Size = New System.Drawing.Size(120, 28)
+        Me.Shape7.TabIndex = 1
+        Me.Shape7.UseVisualStyleBackColor = False
+        '
+        'Shape2
+        '
+        Me.Shape2.BackColor = System.Drawing.Color.White
+        Me.Shape2.Cursor = System.Windows.Forms.Cursors.No
+        Me.Shape2.Location = New System.Drawing.Point(199, 66)
+        Me.Shape2.Name = "Shape2"
+        Me.Shape2.Size = New System.Drawing.Size(28, 120)
+        Me.Shape2.TabIndex = 2
+        Me.Shape2.UseVisualStyleBackColor = False
+        '
+        'Shape3
+        '
+        Me.Shape3.BackColor = System.Drawing.Color.White
+        Me.Shape3.Cursor = System.Windows.Forms.Cursors.No
+        Me.Shape3.Location = New System.Drawing.Point(199, 206)
+        Me.Shape3.Name = "Shape3"
+        Me.Shape3.Size = New System.Drawing.Size(28, 120)
+        Me.Shape3.TabIndex = 3
+        Me.Shape3.UseVisualStyleBackColor = False
+        '
+        'Shape4
+        '
+        Me.Shape4.BackColor = System.Drawing.Color.White
+        Me.Shape4.Cursor = System.Windows.Forms.Cursors.No
+        Me.Shape4.Location = New System.Drawing.Point(80, 322)
+        Me.Shape4.Name = "Shape4"
+        Me.Shape4.Size = New System.Drawing.Size(120, 28)
+        Me.Shape4.TabIndex = 4
+        Me.Shape4.UseVisualStyleBackColor = False
+        '
+        'Shape5
+        '
+        Me.Shape5.BackColor = System.Drawing.Color.White
+        Me.Shape5.Cursor = System.Windows.Forms.Cursors.No
+        Me.Shape5.Location = New System.Drawing.Point(53, 206)
+        Me.Shape5.Name = "Shape5"
+        Me.Shape5.Size = New System.Drawing.Size(28, 120)
+        Me.Shape5.TabIndex = 5
+        Me.Shape5.UseVisualStyleBackColor = False
+        '
+        'Shape6
+        '
+        Me.Shape6.BackColor = System.Drawing.Color.White
+        Me.Shape6.Cursor = System.Windows.Forms.Cursors.No
+        Me.Shape6.Location = New System.Drawing.Point(52, 66)
+        Me.Shape6.Name = "Shape6"
+        Me.Shape6.Size = New System.Drawing.Size(28, 120)
+        Me.Shape6.TabIndex = 6
+        Me.Shape6.UseVisualStyleBackColor = False
+        '
+        'GO
+        '
+        Me.GO.Location = New System.Drawing.Point(52, 371)
+        Me.GO.Name = "GO"
+        Me.GO.Size = New System.Drawing.Size(346, 71)
+        Me.GO.TabIndex = 8
+        Me.GO.Text = "Button1"
+        Me.GO.UseVisualStyleBackColor = True
+        '
+        'RB0
+        '
+        Me.RB0.AutoSize = True
+        Me.RB0.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB0.Location = New System.Drawing.Point(286, 92)
+        Me.RB0.Name = "RB0"
+        Me.RB0.Size = New System.Drawing.Size(54, 20)
+        Me.RB0.TabIndex = 9
+        Me.RB0.Text = "Zero"
+        Me.RB0.UseVisualStyleBackColor = True
+        '
+        'RB1
+        '
+        Me.RB1.AutoSize = True
+        Me.RB1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB1.Location = New System.Drawing.Point(286, 115)
+        Me.RB1.Name = "RB1"
+        Me.RB1.Size = New System.Drawing.Size(51, 20)
+        Me.RB1.TabIndex = 10
+        Me.RB1.TabStop = True
+        Me.RB1.Text = "One"
+        Me.RB1.UseVisualStyleBackColor = True
+        '
+        'RB2
+        '
+        Me.RB2.AutoSize = True
+        Me.RB2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB2.Location = New System.Drawing.Point(286, 138)
+        Me.RB2.Name = "RB2"
+        Me.RB2.Size = New System.Drawing.Size(52, 20)
+        Me.RB2.TabIndex = 11
+        Me.RB2.TabStop = True
+        Me.RB2.Text = "Two"
+        Me.RB2.UseVisualStyleBackColor = True
+        '
+        'RB3
+        '
+        Me.RB3.AutoSize = True
+        Me.RB3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB3.Location = New System.Drawing.Point(286, 161)
+        Me.RB3.Name = "RB3"
+        Me.RB3.Size = New System.Drawing.Size(62, 20)
+        Me.RB3.TabIndex = 12
+        Me.RB3.TabStop = True
+        Me.RB3.Text = "Three"
+        Me.RB3.UseVisualStyleBackColor = True
+        '
+        'RB4
+        '
+        Me.RB4.AutoSize = True
+        Me.RB4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB4.Location = New System.Drawing.Point(286, 184)
+        Me.RB4.Name = "RB4"
+        Me.RB4.Size = New System.Drawing.Size(53, 20)
+        Me.RB4.TabIndex = 13
+        Me.RB4.TabStop = True
+        Me.RB4.Text = "Four"
+        Me.RB4.UseVisualStyleBackColor = True
+        '
+        'RB5
+        '
+        Me.RB5.AutoSize = True
+        Me.RB5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB5.Location = New System.Drawing.Point(286, 210)
+        Me.RB5.Name = "RB5"
+        Me.RB5.Size = New System.Drawing.Size(52, 20)
+        Me.RB5.TabIndex = 14
+        Me.RB5.TabStop = True
+        Me.RB5.Text = "Five"
+        Me.RB5.UseVisualStyleBackColor = True
+        '
+        'RB6
+        '
+        Me.RB6.AutoSize = True
+        Me.RB6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB6.Location = New System.Drawing.Point(286, 233)
+        Me.RB6.Name = "RB6"
+        Me.RB6.Size = New System.Drawing.Size(44, 20)
+        Me.RB6.TabIndex = 15
+        Me.RB6.TabStop = True
+        Me.RB6.Text = "Six"
+        Me.RB6.UseVisualStyleBackColor = True
+        '
+        'RB7
+        '
+        Me.RB7.AutoSize = True
+        Me.RB7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB7.Location = New System.Drawing.Point(286, 256)
+        Me.RB7.Name = "RB7"
+        Me.RB7.Size = New System.Drawing.Size(65, 20)
+        Me.RB7.TabIndex = 16
+        Me.RB7.TabStop = True
+        Me.RB7.Text = "Seven"
+        Me.RB7.UseVisualStyleBackColor = True
+        '
+        'RB8
+        '
+        Me.RB8.AutoSize = True
+        Me.RB8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB8.Location = New System.Drawing.Point(286, 279)
+        Me.RB8.Name = "RB8"
+        Me.RB8.Size = New System.Drawing.Size(56, 20)
+        Me.RB8.TabIndex = 17
+        Me.RB8.TabStop = True
+        Me.RB8.Text = "Eight"
+        Me.RB8.UseVisualStyleBackColor = True
+        '
+        'RB9
+        '
+        Me.RB9.AutoSize = True
+        Me.RB9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB9.Location = New System.Drawing.Point(286, 302)
+        Me.RB9.Name = "RB9"
+        Me.RB9.Size = New System.Drawing.Size(54, 20)
+        Me.RB9.TabIndex = 18
+        Me.RB9.TabStop = True
+        Me.RB9.Text = "Nine"
+        Me.RB9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkGray
+        Me.ClientSize = New System.Drawing.Size(434, 498)
+        Me.Controls.Add(Me.RB9)
+        Me.Controls.Add(Me.RB8)
+        Me.Controls.Add(Me.RB7)
+        Me.Controls.Add(Me.RB6)
+        Me.Controls.Add(Me.RB5)
+        Me.Controls.Add(Me.RB4)
+        Me.Controls.Add(Me.RB3)
+        Me.Controls.Add(Me.RB2)
+        Me.Controls.Add(Me.RB1)
+        Me.Controls.Add(Me.RB0)
+        Me.Controls.Add(Me.GO)
+        Me.Controls.Add(Me.Shape6)
+        Me.Controls.Add(Me.Shape5)
+        Me.Controls.Add(Me.Shape4)
+        Me.Controls.Add(Me.Shape3)
+        Me.Controls.Add(Me.Shape2)
+        Me.Controls.Add(Me.Shape7)
+        Me.Controls.Add(Me.Shape1)
+        Me.Name = "Form1"
+        Me.Padding = New System.Windows.Forms.Padding(10)
+        Me.Text = "SevenSegment by Darlyn Nunez"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Shape1 As Button
+    Friend WithEvents Shape7 As Button
+    Friend WithEvents Shape2 As Button
+    Friend WithEvents Shape3 As Button
+    Friend WithEvents Shape4 As Button
+    Friend WithEvents Shape5 As Button
+    Friend WithEvents Shape6 As Button
+    Friend WithEvents GO As Button
+    Friend WithEvents RB0 As RadioButton
+    Friend WithEvents RB1 As RadioButton
+    Friend WithEvents RB2 As RadioButton
+    Friend WithEvents RB3 As RadioButton
+    Friend WithEvents RB4 As RadioButton
+    Friend WithEvents RB5 As RadioButton
+    Friend WithEvents RB6 As RadioButton
+    Friend WithEvents RB7 As RadioButton
+    Friend WithEvents RB8 As RadioButton
+    Friend WithEvents RB9 As RadioButton
+End Class
---
+++ Nunez.SevenSegment/Nunez.SevenSegment/Form1.vb
@@ -0,0 +1,139 @@
+﻿Public Class Form1
+	Dim ButtonClickCount = 0
+	Private Sub GO_Click(sender As Object, e As EventArgs) Handles GO.Click
+		ButtonClickCount += 1
+
+		If ButtonClickCount = 1 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.Red
+			Shape5.BackColor = Color.Red
+			Shape6.BackColor = Color.Red
+			Shape7.BackColor = Color.White
+			RB0.Checked = True
+		End If
+
+		If ButtonClickCount = 2 Then
+			Shape1.BackColor = Color.White
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.White
+			Shape6.BackColor = Color.White
+			Shape5.BackColor = Color.White
+			Shape7.BackColor = Color.White
+			RB1.Checked = True
+		End If
+
+		If ButtonClickCount = 3 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.White
+			Shape4.BackColor = Color.Red
+			Shape5.BackColor = Color.Red
+			Shape6.BackColor = Color.White
+			Shape7.BackColor = Color.Red
+			RB2.Checked = True
+		End If
+
+		If ButtonClickCount = 4 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.Red
+			Shape5.BackColor = Color.White
+			Shape6.BackColor = Color.White
+			Shape7.BackColor = Color.Red
+			RB3.Checked = True
+		End If
+
+		If ButtonClickCount = 5 Then
+			Shape1.BackColor = Color.White
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.White
+			Shape5.BackColor = Color.White
+			Shape6.BackColor = Color.Red
+			Shape7.BackColor = Color.Red
+			RB4.Checked = True
+		End If
+
+		If ButtonClickCount = 6 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.White
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.Red
+			Shape5.BackColor = Color.White
+			Shape6.BackColor = Color.Red
+			Shape7.BackColor = Color.Red
+			RB5.Checked = True
+		End If
+
+		If ButtonClickCount = 7 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.White
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.Red
+			Shape5.BackColor = Color.Red
+			Shape6.BackColor = Color.Red
+			Shape7.BackColor = Color.Red
+			RB6.Checked = True
+		End If
+
+		If ButtonClickCount = 8 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.White
+			Shape5.BackColor = Color.White
+			Shape6.BackColor = Color.White
+			Shape7.BackColor = Color.White
+			RB7.Checked = True
+		End If
+
+		If ButtonClickCount = 9 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.Red
+			Shape5.BackColor = Color.Red
+			Shape6.BackColor = Color.Red
+			Shape7.BackColor = Color.Red
+			RB8.Checked = True
+		End If
+
+		If ButtonClickCount = 10 Then
+			Shape1.BackColor = Color.Red
+			Shape2.BackColor = Color.Red
+			Shape3.BackColor = Color.Red
+			Shape4.BackColor = Color.Red
+			Shape5.BackColor = Color.White
+			Shape6.BackColor = Color.Red
+			Shape7.BackColor = Color.Red
+			RB9.Checked = True
+		End If
+
+		If ButtonClickCount > 10 Then
+			ButtonClickCount = 0
+			Shape1.BackColor = Color.White
+			Shape2.BackColor = Color.White
+			Shape3.BackColor = Color.White
+			Shape4.BackColor = Color.White
+			Shape6.BackColor = Color.White
+			Shape5.BackColor = Color.White
+			Shape7.BackColor = Color.White
+			RB1.Checked = False
+			RB2.Checked = False
+			RB3.Checked = False
+			RB4.Checked = False
+			RB5.Checked = False
+			RB6.Checked = False
+			RB7.Checked = False
+			RB8.Checked = False
+			RB9.Checked = False
+			RB0.Checked = False
+		End If
+
+	End Sub
+
+End Class
